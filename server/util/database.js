520f40f8290e551c69460980cd1ee89e9f7028e7
saicharanars <saicharan_@outlook.com>
2023-07-01 21:33:56 +0530

    database upadate

--- server/util/database.js
+++ server/util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node_database','root','724242726',{
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
   dialect:'mysql',
-  host:'localhost'
+  host:process.env.DB_HOST,
 });
 
 module.exports=sequelize;
